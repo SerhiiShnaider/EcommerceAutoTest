e7267d20d099c1124f074064bbaef49664f058b8
SerhiS <serhioeskobar@gmail.com>
2018-09-16 23:13:11 +0300

    added ie support

--- src/main/java/com/gmail/shnapi007/config/factory/DriverFactory.java
+++ src/main/java/com/gmail/shnapi007/config/factory/DriverFactory.java
@@ -1,13 +1,16 @@
 package com.gmail.shnapi007.config.factory;
 
 import com.gmail.shnapi007.config.factory.impl.ChromeDriverData;
+import com.gmail.shnapi007.config.factory.impl.InternetExplorerDriverData;
 
 public class DriverFactory {
 
-  public static DriverData gerDriverData(String s) {
+  public static DriverData gerDriverData(String driverName) {
     DriverData driverData = null;
-    if (s.equalsIgnoreCase("chrome")) {
+    if (driverName.equalsIgnoreCase("chrome")) {
       driverData = new ChromeDriverData();
+    }else if (driverName.equalsIgnoreCase("ie")){
+      driverData = new InternetExplorerDriverData();
     }
     return driverData;
   }
---
+++ src/main/java/com/gmail/shnapi007/config/factory/impl/InternetExplorerDriverData.java
@@ -0,0 +1,28 @@
+package com.gmail.shnapi007.config.factory.impl;
+
+import com.gmail.shnapi007.config.factory.DriverData;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.ie.InternetExplorerDriver;
+import org.openqa.selenium.remote.DesiredCapabilities;
+
+public class InternetExplorerDriverData implements DriverData {
+
+  @Override
+  public String driverName() {
+    return "webdriver.ie.driver";
+  }
+
+  @Override
+  public String driverRef() {
+    return "src/main/resources/drivers/IEDriverServer.exe";
+  }
+
+  @Override
+  public WebDriver driver() {
+    DesiredCapabilities capabilities = DesiredCapabilities.internetExplorer();
+    capabilities
+        .setCapability(InternetExplorerDriver.INTRODUCE_FLAKINESS_BY_IGNORING_SECURITY_DOMAINS,
+            true);
+    return new InternetExplorerDriver(capabilities);
+  }
+}
