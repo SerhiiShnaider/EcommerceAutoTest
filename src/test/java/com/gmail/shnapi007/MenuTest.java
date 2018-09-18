fc90e05d39d7a42a5712c16ef3721ed596990e7f
Serhii_Shnaider <Serhii_Shnaider@epam.com>
2018-09-18 12:26:19 +0300

    added allure report

--- src/test/java/com/gmail/shnapi007/MenuTest.java
+++ src/test/java/com/gmail/shnapi007/MenuTest.java
@@ -1,18 +1,25 @@
 package com.gmail.shnapi007;
 
-import org.apache.log4j.Logger;
+import io.qameta.allure.Description;
+import io.qameta.allure.Story;
+import org.testng.Assert;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.Test;
 
 public class MenuTest {
 
   @Test
-  public void test() {
-    Logger logger = Logger.getLogger(MenuTest.class);
+  @Description(value = "test Test")
+  @Story(value = "AYEN-1488")
+  public void acceptTest() {
+    Assert.assertTrue(4 + 4 == 8, "accept");
+  }
 
-    logger.info("info");
-    logger.warn("info");
-    logger.error("error");
+  @Test
+  @Description(value = "test Test")
+  @Story(value = "AYEN-1488")
+  public void falseTest() {
+    Assert.assertTrue(4 + 4 == 7, "false");
   }
 
   @AfterMethod
